bcb73b10e9222a4f16e572fc5bdadb0a8eb1d25e
曾纪文 <jiwenzeng@123u.com>
2021-06-07 23:19:15 +0800

    change go mod name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module gamenet
+module github.com/zengjiwen/gamenet
 
-go 1.13
+go 1.14
--- server/server.go
+++ server/server.go
@@ -2,7 +2,7 @@ package server
 
 import (
 	"fmt"
-	"gamenet"
+	"github.com/zengjiwen/gamenet"
 	"strings"
 )
 
